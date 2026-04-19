c09d3c848d96d510d8bd9f6696d78242b48778b9
vjb <vjb@users.noreply.github.com>
2026-04-19 18:56:30 -0400

    chore: clean up obsolete configuration files and scratch directory from root

--- push2.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "docs: update README with final UI screenshots and phase descriptions"
-git push https://github.com/vjb/quorom-debate master
--- scratch/check-ffmpeg.js
+++
@@ -1,4 +0,0 @@
-const ffmpeg = require('ffmpeg-static');
-const fs = require('fs');
-console.log('FFMPEG Path:', ffmpeg);
-console.log('Exists:', fs.existsSync(ffmpeg));
